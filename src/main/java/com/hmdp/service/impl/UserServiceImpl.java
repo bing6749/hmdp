d0d4eb471288d7122e553b2ed1f3bde2f8e7981c
MaRui <bing6749@163.com>
2024-07-29 15:38:40 +0800

    first commit

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,84 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号格式是否正确
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式不正确");
+        }
+        //生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //将验证码存入session
+        session.setAttribute("code", code);
+        //发送验证码
+        // TODO 发送验证码,调用阿里云短信服务
+        log.debug("向手机号{}发送验证码：{}", phone, code);
+        //返回成功
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号格式是否正确
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+            return Result.fail("手机号格式不正确");
+        }
+        //校验验证码是否正确
+        String code = loginForm.getCode();
+        if (RegexUtils.isCodeInvalid(code)) {
+            return Result.fail("验证码格式不正确");
+
+        }
+        Object cacheCode = session.getAttribute("code");
+        if (cacheCode == null || !cacheCode.equals(code)) {
+            return Result.fail("验证码错误");
+        }
+        //判断用户是否存在
+        User user = query().eq("phone", loginForm.getPhone()).one();
+
+        //不存在，创建用户信息对象
+        if (user == null) {
+            user = createUserWithPhone(loginForm.getPhone());
+        }
+        //取出用户信息放入session
+        session.setAttribute("user", user);
+        //返回成功
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
+}
